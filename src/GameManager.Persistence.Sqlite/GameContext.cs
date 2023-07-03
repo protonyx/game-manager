0450d8f6653c8637b2a38b6eafad91f1a6e380e4
Protonyx <protonyx@users.noreply.github.com>
2023-07-02 22:57:10 -0600

    fix: treat all DateTime properties in the database as UTC

--- src/GameManager.Persistence.Sqlite/GameContext.cs
+++ src/GameManager.Persistence.Sqlite/GameContext.cs
@@ -1,5 +1,6 @@
 using GameManager.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GameManager.Persistence.Sqlite;
 
@@ -26,6 +27,35 @@ public class GameContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        
+        // Treat all DateTime properties as UTC
+        var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+            v => v.ToUniversalTime(),
+            v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+        var nullableDateTimeConverter = new ValueConverter<DateTime?, DateTime?>(
+            v => v.HasValue ? v.Value.ToUniversalTime() : v,
+            v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v);
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            if (entityType.IsKeyless)
+            {
+                continue;
+            }
+
+            foreach (var property in entityType.GetProperties())
+            {
+                if (property.ClrType == typeof(DateTime))
+                {
+                    property.SetValueConverter(dateTimeConverter);
+                }
+                else if (property.ClrType == typeof(DateTime?))
+                {
+                    property.SetValueConverter(nullableDateTimeConverter);
+                }
+            }
+        }
         
         base.OnModelCreating(modelBuilder);
     }
