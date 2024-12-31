6c9f1c5b8d665c25e5fe7f78be36ebd80a8f8e5c
Protonyx <protonyx@users.noreply.github.com>
2024-12-31 16:45:06 -0500

    aspire support for angular app

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -6,6 +6,14 @@ var cache = builder.AddRedis("cache")
     .WithRedisInsight();
 
 var api = builder.AddProject<GameManager_Server>("api")
-    .WithReference(cache);
+    .WithReference(cache)
+    .WaitFor(cache);
+
+var app = builder.AddNpmApp("angular", "../../web")
+    .WithReference(api)
+    .WaitFor(api)
+    .WithHttpEndpoint(env: "PORT", port: 4200)
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
